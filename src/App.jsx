10798d053ac32c72ab67d8daac450bf9b7fa87ff
cdhome <jordansewpershad@outlook.com>
2025-02-18 12:11:29 +0200

    Header componnent added

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import './App.css'
+import Header from './components/Header'
 
 function App() {
 
     return (
-      <h1 className='header'>Yes Chef!</h1>
+      <>
+        <Header/>
+      </>
     )
   }
   

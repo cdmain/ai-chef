bb7408ae05618f1176b9951fb96f46abb75f7114
cdhome <jordansewpershad@outlook.com>
2025-02-24 14:34:42 +0200

    added useState and manage state of the ingredients list

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,15 +1,34 @@
+import { useState } from "react"
+
 export default function Main() {
+
+    const [ingredients, setIngredients] = useState([])
+
+    const ingredientsListItems = ingredients.map(ingredient => (
+        <li key={ingredient}>{ingredient}</li>
+    ))
+
+    function addIngredients(formData) {
+        const newIngredient = formData.get("ingredient")
+        
+        setIngredients(prevIngredientsList => [...prevIngredientsList, newIngredient])
+    }
+
     return (
         <main className="page">
-            <form className="form">
+            <form action={addIngredients} className="add-ingredient-form">
                 <input 
                     className="ingredient-input" 
                     aria-label="Add ingredient"
                     type="text" 
-                    placeholder="e.g. oregano" 
+                    placeholder="e.g. oregano"
+                    name="ingredient"
                 />
                 <button className="add-ingredient-button">Add ingredient</button>
             </form>
+            <ul className="ingredients-list">
+                {ingredientsListItems}
+            </ul>
         </main>
     )
 }

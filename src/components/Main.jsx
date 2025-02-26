a34827bc1976050b7e629c8d066ab04e70eb5856
cdhome <jordansewpershad@outlook.com>
2025-02-26 15:47:00 +0200

    added conditional rendering for the ingredients list

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -11,7 +11,8 @@ export default function Main() {
     function addIngredients(formData) {
         const newIngredient = formData.get("ingredient")
         
-        setIngredients(prevIngredientsList => [...prevIngredientsList, newIngredient])
+        if (newIngredient !== "")
+            setIngredients(prevIngredientsList => [...prevIngredientsList, newIngredient])
     }
 
     return (
@@ -26,9 +27,25 @@ export default function Main() {
                 />
                 <button className="add-ingredient-button">Add ingredient</button>
             </form>
-            <ul className="ingredients-list">
-                {ingredientsListItems}
-            </ul>
+
+            {ingredients.length > 0 && <section className="ingredients-container">
+                
+                <div className="ingredients-list-container">
+                    <h2 className="ingredients-list-heading">Ingredients on hand:</h2>
+                    <ul className="ingredients-list">
+                        {ingredientsListItems}
+                    </ul>
+                </div>
+
+                <div className="get-recipe-container">
+                    <div>
+                        <h3 className="get-recipe-heading">Ready for a recipe?</h3>
+                        <p className="get-recipe-explaination">Generate a recipe from you list of ingredients.</p>
+                    </div>
+                    <button className="get-recipe-button">Get a recipe</button>                    
+                </div>
+
+            </section>}
         </main>
     )
 }

dad18348fb484e25ae430b74097cad0cca76719a
cdhome <jordansewpershad@outlook.com>
2025-02-27 10:27:26 +0200

    Updated Main.jsx added conditional rendering for the get recipe button (will only display if the ingredients list is > 3)

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -4,13 +4,17 @@ export default function Main() {
 
     const [ingredients, setIngredients] = useState([])
 
+    // Maps over the ingredients list and wraps each element with list item tags <li>...</li>
     const ingredientsListItems = ingredients.map(ingredient => (
         <li key={ingredient}>{ingredient}</li>
     ))
 
+    // Updates ingredients list when user enters a new ingredient (using form data)
     function addIngredients(formData) {
+        
         const newIngredient = formData.get("ingredient")
         
+        // Do not allow empty strings to be added to the ingredients list
         if (newIngredient !== "")
             setIngredients(prevIngredientsList => [...prevIngredientsList, newIngredient])
     }
@@ -28,7 +32,7 @@ export default function Main() {
                 <button className="add-ingredient-button">Add ingredient</button>
             </form>
 
-            {ingredients.length > 0 && <section className="ingredients-container">
+            { ingredients.length > 0 && <section className="ingredients-container">
                 
                 <div className="ingredients-list-container">
                     <h2 className="ingredients-list-heading">Ingredients on hand:</h2>
@@ -37,15 +41,17 @@ export default function Main() {
                     </ul>
                 </div>
 
-                <div className="get-recipe-container">
+                { ingredients.length > 3 && <div className="get-recipe-container">
                     <div>
                         <h3 className="get-recipe-heading">Ready for a recipe?</h3>
                         <p className="get-recipe-explaination">Generate a recipe from you list of ingredients.</p>
                     </div>
                     <button className="get-recipe-button">Get a recipe</button>                    
                 </div>
+                }
 
-            </section>}
+            </section>
+            }
         </main>
     )
 }

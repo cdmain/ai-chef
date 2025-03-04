90cde6cf729be6346bdcd02a3de8172c5faf8256
cdhome <jordansewpershad@outlook.com>
2025-03-04 12:33:30 +0200

    Rebranding to AI Chef

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,10 @@
-import chefClaudeIcon from "/src/assets/Chef-Claude-Icon.png"
+import AIChefIcon from "/src/assets/AI-Chef-Icon.png"
 
 export default function Header() {
     return (
         <nav className="nav-bar">
-            <img className="nav-icon" src={chefClaudeIcon} alt="Chef Claude Icon" />
-            <span className="nav-title">Chef Claude</span>
+            <img className="nav-icon" src={AIChefIcon} alt="AI Chef Icon" />
+            <span className="nav-title">AI Chef</span>
         </nav>
     )
 }
